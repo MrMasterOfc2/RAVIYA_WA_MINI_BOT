0e126a4cf032682cb2d050fdc77ebcda527dc1b7
Sahan Maduwantha <madumax2006@gmail.com>
2026-01-30 14:59:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const express = require('express');
+const path = require('path');  // Add this
 const app = express();
-__path = process.cwd()
+const __path = path.join(process.cwd(), '/');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
+
+let code = require('./pair');  // Ensure pair.js exists
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
+    res.sendFile(path.join(__path, 'pair.html'));
 });
 app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+    res.sendFile(path.join(__path, 'main.html'));
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star ‼️
-
-> POWERD BY DTZ RAVIYA
-
-Server running on http://localhost:` + PORT)
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
